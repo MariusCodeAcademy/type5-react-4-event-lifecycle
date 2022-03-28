28f439c6083bbe9077a9f02116bd1fbafd0da03a
Marius <marius@example.com>
2022-03-28 14:59:35 +0300

    login logout uzd

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import Counter from './components/counter/Counter';
 
 function App() {
+  const isUserLoggedIn = true;
   return (
     <div className='App'>
       <h1>App</h1>
       <Counter />
+      <LoginLogout status={isUserLoggedIn} />
     </div>
   );
 }
--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,21 +1,53 @@
+import { useState } from 'react';
 import './counter.css';
 
 function Counter() {
   console.log('Counter component ran');
   // useState
-  let count = 5;
+  const [count, setCount] = useState(5);
+  // let count = 5;
   function downHandler() {
     console.log('Down button was clicked');
-    count--;
+    // count--; // niekada tiesiogiia state nekeiciam
+    setCount(count - 1);
     console.log('count ===', count);
   }
 
+  const upHandler = () => {
+    setCount(count + 1);
+  };
+
+  const upFiveHandler = function () {
+    setCount(count + 5);
+  };
+
+  const dynamicClass = count < 0 ? 'negative' : '';
+  const greenClass = count > 20 ? 'green' : '';
+
+  const calcClass = () => {
+    if (count < 0) {
+      return 'negative';
+    }
+    if (count > 20) {
+      return 'green';
+    }
+    return '';
+  };
+
+  // prideti mygtuka +5 kuri paspaudus count padidinama 5 taskais
+  // padaryti kad jei reikme daugiau uz 20 tai jos spalva zalia
+
   return (
     <div className='counter'>
-      <h2>{count}</h2>
+      <h2 className={calcClass()}>{count}</h2>
       <div className='control'>
-        <button onClick={() => console.log('up was clicked')}>UP</button>
-        <button onClick={downHandler}>DOWN</button>
+        <button onClick={upHandler}>UP</button>
+        <button onClick={upFiveHandler}>UP +5</button>
+        {count > -5 && (
+          <button disabled={count === -5} onClick={downHandler}>
+            DOWN
+          </button>
+        )}
       </div>
     </div>
   );
